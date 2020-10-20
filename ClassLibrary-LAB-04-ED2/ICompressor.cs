43ea8248aa3b2455892f2c42eb4d3dd5f3bc01d0
jamg1906 <jamg1906@gmail.com>
2020-10-19 19:03:41 -0600

    LZW class & Compressor interface added

---
+++ ClassLibrary-LAB-04-ED2/ICompressor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary_LAB_04_ED2
+{
+    interface ICompressor
+    {
+        public byte[] Compression(byte[] NonCompressedText);
+        public byte[] Descompresion(byte[] LZWCompressedText);
+    }
+}
---
+++ ClassLibrary-LAB-04-ED2/LZW.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary_LAB_04_ED2
+{
+    public class LZW : ICompressor
+    {
+    }
+}
